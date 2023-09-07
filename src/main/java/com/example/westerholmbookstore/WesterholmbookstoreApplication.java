5e1b45a2a9a76abeec8a63f8ca0c81957003667f
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-09-07 08:38:57 +0300

    first commit

---
+++ src/main/java/com/example/westerholmbookstore/WesterholmbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.westerholmbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesterholmbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesterholmbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/westerholmbookstore/WesterholmbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.westerholmbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesterholmbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
