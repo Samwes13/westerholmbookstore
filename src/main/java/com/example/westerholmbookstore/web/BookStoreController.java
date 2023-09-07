840dfdd153e45e64489cdaed21c2cea12542e6ff
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-09-07 12:22:20 +0300

    main

---
+++ src/main/java/com/example/westerholmbookstore/web/BookStoreController.java
@@ -0,0 +1,15 @@
+package com.example.westerholmbookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookStoreController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        
+        return "index";
+    }
+}
